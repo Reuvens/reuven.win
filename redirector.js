999ca2c703546e83516aedd290c15dd201ca4218
Reuven Sayag <reuven.sa@gmail.com>
2020-11-07 19:15:01 +0200

    Update redirector.js

--- redirector.js
+++ redirector.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	var redirects_dict = loadRedirects();
 	
 	const url = new URL(window.location.href);
-	var source_name = url.search.slice(2);
+	var source_name = url.search.slice(1);
 	if (redirects_dict[source_name]) {
 		var destination = redirects_dict[source_name].destination;
 		console.log(destination);
